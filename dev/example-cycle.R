8dc03111d22fa0a8c3bd5e03180e3ea691e199f3
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2023-02-08 09:43:57 +0100

    dev

--- dev/example-cycle.R
+++ dev/example-cycle.R
@@ -3,14 +3,15 @@ sets <- create_profiles(
   1,
   load_increment = 2.5,
   v1RM = 0.3,
-  biological_variation = 0,
-  L0_fatigue = -10,
+  biological_variation = 0.1,
+  L0_fatigue = 0,
   L0_fatigue_multiplicative = FALSE,
-  L0_visit_random = 10) %>%
+  L0_visit_random = 0) %>%
   create_visits(1:3) %>%
   #create_prescription_1RM() %>%
-  create_sets(load = rep(120, 10), use_true_velocity = FALSE) %>%
+  create_sets(load = rep(120, 10),  reps = 5, use_true_velocity = FALSE) %>%
   create_summary()
+
 
 plot(sets) +
   ggplot2::geom_hline(yintercept = 0.3)
