0f758c1f5fdc7e61ab169d5637214afa8533d751
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2023-05-01 08:02:34 +0200

    Added additive and multiplicative effects

--- R/utils-reps-velocity.R
+++ R/utils-reps-velocity.R
@@ -3,25 +3,15 @@
 # Function provide Exertion-Velocity profile utilizing \code{V0_rep_drop} and
 #     \code{L0_rep_drop} parameters
 #
-# @param V0 Numeric
-# @param V0_rep_drop Numeric
-# @param L0 Numeric
-# @param L0_rep_drop Numeric
-# @param biological_variation Numeric
-# @param biological_variation_multiplicative Logical
-# @param instrumentation_noise Numeric
-# @param instrumentation_noise_multiplicative Logical
-# @param rep Numeric
-# @param load Numeric
-#
-# @return Data frame
 get_reps_velocity <- function(V0,
-                              V0_rep_drop,
+                              V0_rep_drop_additive,
+                              V0_rep_drop_multiplicative,
                               L0,
-                              L0_rep_drop,
-                              biological_variation,
+                              L0_rep_drop_additive,
+                              L0_rep_drop_multiplicative,
+                              biological_variation_additive,
                               biological_variation_multiplicative,
-                              instrumentation_noise,
+                              instrumentation_noise_additive,
                               instrumentation_noise_multiplicative,
                               rep,
                               load) {
@@ -37,26 +27,30 @@ get_reps_velocity <- function(V0,
 
   reps <- dplyr::tibble(
     V0 = V0,
-    V0_rep_drop = V0_rep_drop,
+    V0_rep_drop_additive = V0_rep_drop_additive,
+    V0_rep_drop_multiplicative = V0_rep_drop_multiplicative,
     L0 = L0,
-    L0_rep_drop = L0_rep_drop,
+    L0_rep_drop_additive = L0_rep_drop_additive,
+    L0_rep_drop_multiplicative = L0_rep_drop_multiplicative,
     rep = rep,
     load = load
   ) %>%
     dplyr::mutate(
       # Effects of fatigue within-set (i.e., between reps)
       rep_initial_velocity = get_velocity_at_load(V0, L0, load),
-      rep_V0 = V0 - V0_rep_drop * (V0 - rep_initial_velocity) * (rep - 1),
-      rep_L0 = L0 - L0_rep_drop * load * (rep - 1),
+      rep_V0 = V0 - V0_rep_drop_multiplicative * (V0 - rep_initial_velocity) * (rep - 1) - V0_rep_drop_additive * (rep - 1),
+      rep_L0 = L0 - L0_rep_drop_multiplicative * load * (rep - 1) - L0_rep_drop_additive * (rep - 1),
 
       # True profile velocity
       true_rep_velocity = get_velocity_at_load(rep_V0, rep_L0, load),
 
       # Add biological variation
-      biological_rep_velocity = random_effect(true_rep_velocity, 1, biological_variation, biological_variation_multiplicative),
+      biological_rep_velocity = random_effect(true_rep_velocity, 1, biological_variation_multiplicative, TRUE),
+      biological_rep_velocity = random_effect(biological_rep_velocity, 1, biological_variation_additive, FALSE),
 
       # Add instrumentation noise
-      measured_rep_velocity = random_effect(biological_rep_velocity, 1, instrumentation_noise, instrumentation_noise_multiplicative)
+      measured_rep_velocity = random_effect(biological_rep_velocity, 1, instrumentation_noise_multiplicative, TRUE),
+      measured_rep_velocity = random_effect(measured_rep_velocity, 1, instrumentation_noise_additive, FALSE)
     )
 
   return(reps)
