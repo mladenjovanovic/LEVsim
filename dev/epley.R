85468d329342fdf0c0807151768f929b022eeaae
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2023-05-09 10:58:41 +0200

    Play with the new rep drop

--- dev/epley.R
+++ dev/epley.R
@@ -3,9 +3,9 @@ require(LEVsim)
 require(STMr)
 
 params <- expand_grid(
-  L0 = seq(120, 250, length.out = 4),
-  V0 = seq(1.5, 2.5, length.out = 4),
-  v1RM = seq(0.05, 0.4, length.out = 4)
+  L0 = 200,
+  V0 = 1.8,
+  v1RM = 0.2
 ) %>%
   mutate(athlete = seq(1, n()))
 
@@ -19,8 +19,8 @@ RTF_func <- function(x) {
     v1RM = params$v1RM,
 
     # fatigue effects
-    V0_rep_drop_multiplicative = x[1],
-    L0_rep_drop_multiplicative = x[2])
+    V0_rep_drop = x[1],
+    L0_rep_drop = x[2])
 
   RTF <- profiles %>%
     create_visits(1) %>%
@@ -42,33 +42,65 @@ RTF_func <- function(x) {
 }
 
 res <- optim(
-  par = c(0, 0.05),
+  par = c(0.05, 0.05),
   RTF_func,
   method = "L-BFGS-B",
-  lower = c(0.0, 0.01),
-  upper = c(0.1, 0.1),
-  control = list(trace = TRUE))
+  lower = c(0, 0.03),
+  upper = c(0.2, 0.1),
+  control = list(trace = 100))
 
 stop()
-# Manual
-man <- tibble(
-  param = seq(2, 6, length.out = 30) / 100
-) %>%
-  rowwise() %>%
-  mutate(
-    RMSE = RTF_func(param)
-  )
 
-ggplot(man, aes(x = param, y = RMSE)) +
-  theme_linedraw() +
-  geom_point() +
-  geom_line()
-
-RTF_func(c(0.04476617))
 
 # Only L0
 0.04476617
 0.04451621
 
 # V0 and L0
-7.018543e-05 4.547028e-02
+7.018543e-05
+4.547028e-02
+
+# New shape of the curve
+
+# Only additive / deccelerating
+0.04244762 # par
+20.47 # % error
+
+# Only multiplicative / accelerating
+0.05345957 # par
+16.70027 # error
+
+# Only linear
+0.04548258 # param
+16.98229 # res
+
+# Latest linear
+0.0417001 # par
+
+# Both
+
+# Plot
+create_profiles(
+  athlete = params$athlete,
+  L0 = params$L0,
+  V0 = params$V0,
+  v1RM = params$v1RM,
+
+  # fatigue effect
+  V0_rep_drop = 0.038, #0.03446838,
+  L0_rep_drop = 0.038) %>%#0.03781305) %>%
+  create_visits(1) %>%
+  create_visit_1RM() %>%
+  create_sets(load = loads_perc, load_type = "visit 1RM", max_reps = 200) %>%
+  create_summary() %>%
+  as.data.frame() %>%
+  select(athlete, load_index, load_perc, nRM) %>%
+  mutate(
+    pred_nRM = STMr::max_reps_epley(load_perc),
+    diff = pred_nRM - nRM,
+    diff_perc = diff / nRM) %>%
+  na.omit() %>%
+  ggplot(aes(x = load_perc, y = nRM, group = athlete)) +
+  geom_line() +
+  geom_point() +
+  geom_line(aes(y = pred_nRM), color = "red")
