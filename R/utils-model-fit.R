da3e2184ed49cdcebcf1b4fc3afd36c38cbc134f
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2023-09-23 07:07:19 +0200

    Added function for providing model fit metrics

---
+++ R/utils-model-fit.R
@@ -0,0 +1,23 @@
+#' Function to return model fit metrics
+LEV_model_fit <- function(model = NULL, observed, predicted, na.rm = FALSE) {
+  resid <- observed - predicted
+
+  # This is needed for the LOOCV option which doesn't return model
+  # thus, RSE cannot be computed
+  if (is.null(model)) {
+    RSE <- NA
+  } else {
+    RSE <- summary(model)$sigma
+  }
+
+  list(
+    RSE = RSE,
+    R_squared = stats::cor(observed, predicted)^2,
+    minErr = min(resid, na.rm = na.rm),
+    maxErr = max(resid, na.rm = na.rm),
+    maxAbsErr = max(abs(resid), na.rm = na.rm),
+    RMSE = sqrt(mean(resid^2, na.rm = na.rm)),
+    MAE = mean(abs(resid), na.rm = na.rm),
+    MAPE = 100 * mean(abs(resid / observed), na.rm = na.rm)
+  )
+}
